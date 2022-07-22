da2c8c22a3f8c63a22b17fec09cb3e17a9ccb6fa
wxwwxk <49890588+wxwwxk@users.noreply.github.com>
2022-07-22 16:04:33 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("=========================");//第一次添加
 
         System.out.println("=========================");//第二次添加
+
     }
 }
